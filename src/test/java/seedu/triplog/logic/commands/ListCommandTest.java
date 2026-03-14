c95f9efe2f1facfcfa2bb8d6874afbebf58b89a8
Derrick Low <lowderrickym@gmail.com>
2026-03-14 23:45:14 +0800

    add tests for listing trips by dates

--- src/test/java/seedu/triplog/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/triplog/logic/commands/ListCommandTest.java
@@ -1,16 +1,19 @@
 package seedu.triplog.logic.commands;
 
-import static seedu.triplog.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.triplog.logic.commands.CommandTestUtil.showTripAtIndex;
-import static seedu.triplog.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
-import static seedu.triplog.testutil.TypicalTrips.getTypicalTripLog;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static seedu.triplog.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.triplog.logic.commands.CommandTestUtil.showTripAtIndex;
 import seedu.triplog.model.Model;
 import seedu.triplog.model.ModelManager;
 import seedu.triplog.model.UserPrefs;
+import seedu.triplog.model.trip.Trip;
+import static seedu.triplog.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.triplog.testutil.TypicalTrips.getTypicalTripLog;
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
@@ -35,5 +38,18 @@ public class ListCommandTest {
     public void execute_listIsFiltered_showsEverything() {
         showTripAtIndex(model, INDEX_FIRST_PERSON);
         assertCommandSuccess(new ListCommand(), model, ListCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void execute_listIsSorted_showsTripsInAscendingOrder() throws Exception {
+        new ListCommand().execute(model);
+
+        List<Trip> sorted = model.getSortedTripList();
+        for (int i = 0; i < sorted.size() - 1; i++) {
+            assertTrue(
+                !sorted.get(i).getStartDate().value.isAfter(sorted.get(i + 1).getStartDate().value),
+                "Expected trips sorted by start date ascending"
+            );
+        }
     }
 }
