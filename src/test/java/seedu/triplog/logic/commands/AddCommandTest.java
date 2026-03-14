6b0880461c6e9ebafc3bd1c1ab787465bd4b0837
Derrick Low <lowderrickym@gmail.com>
2026-03-14 23:44:50 +0800

    add stub methods for interface methods

--- src/test/java/seedu/triplog/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/triplog/logic/commands/AddCommandTest.java
@@ -10,6 +10,7 @@ import static seedu.triplog.testutil.TypicalTrips.ALICE;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
@@ -155,6 +156,16 @@ public class AddCommandTest {
 
         @Override
         public void updateFilteredTripList(Predicate<Trip> predicate) {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public ObservableList<Trip> getSortedTripList() {
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public void updateSortedTripList(Comparator<Trip> comparator) {
             throw new AssertionError("This method should not be called.");
         }
     }
