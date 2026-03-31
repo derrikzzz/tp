cefebf0298dce66b272033494ea50c14c62476af
ths1959 <ths2891503@gmail.com>
2026-03-31 12:59:51 +0800

    Show sorted summary dashboard on app startup

--- src/main/java/seedu/triplog/ui/MainWindow.java
+++ src/main/java/seedu/triplog/ui/MainWindow.java
@@ -74,6 +74,8 @@ public class MainWindow extends UiPart<Stage> {
         commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
 
+        resultDisplay.setFeedbackToUser(logic.getSummary());
+
         if (logic.getInitialDataLoadError() != null) {
             resultDisplay.setFeedbackToUser(logic.getInitialDataLoadError());
         }
